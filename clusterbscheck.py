5aeebdca16c3cc79b316baa880c19af949d56550
Carl Seelye <cseelye@gmail.com>
2013-03-06 21:29:46 -0700

    Add clusterbscheck, move_volume_to_account scripts

---
+++ clusterbscheck.py
@@ -0,0 +1,259 @@
+#!/usr/bin/python
+
+# This script will show details about xUnknownBlockID
+
+# ----------------------------------------------------------------------------
+# Configuration
+#  These may also be set on the command line
+
+mvip = "0.0.0.0"            # The management VIP of the cluster
+                                    # --mvip
+
+username = "admin"                  # Admin account for the cluster
+                                    # --user
+
+password = "solidfire"              # Admin password for the cluster
+                                    # --pass
+
+ssh_user = "root"               # The username for the nodes
+                                # --ssh_user
+
+ssh_pass = "sf.9012182"          # The password for the nodes
+                                # --ssh_pass
+
+timeout = 90                    # How long to wait for ClusterBSCheck to finish, in min
+                                # --timeout
+
+# ----------------------------------------------------------------------------
+
+import sys,os
+from optparse import OptionParser
+import multiprocessing
+import time
+import re
+import libsf
+from libsf import mylog
+import logging
+import traceback
+import datetime
+import calendar
+
+def NodeThread(node_ip, node_user, node_pass, results, index):
+    #mylog.console.setLevel(logging.DEBUG)
+    try:
+        mylog.debug(node_ip + ": Connecting")
+        #mylog.debug(node_ip + ": index = " + str(index))
+        ssh = libsf.ConnectSsh(node_ip, node_user, node_pass)
+        #mylog.info(node_ip + ": Checking for xUnknownBlockID")
+        command = "zgrep xUnknownBlock /var/log/sf-slice.info* | egrep -o 'bs [0-9]+ .+ sliceID=[0-9]+ lba=[0-9]+' | sort -u"
+        
+        stdin, stdout, stderr = libsf.ExecSshCommand(ssh, command)
+        results[index] = []
+        for line in stdout.readlines():
+            m = re.search("bs\s+(\d+).+data=\[\"(\S+).+sliceID=(\d+)\s+lba=(\d+)", line)
+            if (m):
+                slice_id = m.group(3)
+                lba = m.group(4)
+                block = dict()
+                block_index = -1
+                for i, v in enumerate(results[index]):
+                    if v["sliceID"] == slice_id and v["lba"] == lba:
+                        block = v
+                        block_index = i
+                        break
+                block["lba"] = lba
+                block["sliceID"] = slice_id
+                if "bs" not in block:
+                    block["bs"] = []
+                block["bs"].append(m.group(1))
+                block["blockID"] = m.group(2)
+                
+                if block_index > 0:
+                    results[index][block_index] = block
+                else:
+                    a = results[index]
+                    a.append(block)
+                    results[index] = a
+        
+    except Exception as e:
+        mylog.error(traceback.format_exc())
+        results[index] = False
+
+def WaitForBsCheckThread(node_ip, node_user, node_pass, start_time, timeout, results, index):
+    results[index] = False
+    try:
+        ssh = libsf.ConnectSsh(node_ip, node_user, node_pass)
+        command = "grep 'finished cluster bs check' /var/log/sf-slice.info | tail"
+        pattern = re.compile("^(?P<month>[a-zA-Z]{3})\s+(?P<day>\d\d?)\s(?P<hour>\d\d)\:(?P<minute>\d\d):(?P<second>\d\d)(?:\s(?P<suppliedhost>[a-zA-Z0-9_-]+))?\s(?P<host>[a-zA-Z0-9_-]+)\s(?P<process>[a-zA-Z0-9\/_-]+)(\[(?P<pid>\d+)\])?:\s(?P<message>.+)$")
+        finished = False
+        while not finished:
+            stdin, stdout, stderr = libsf.ExecSshCommand(ssh, command)
+            for line in stdout.readlines():
+                m = pattern.match(line)
+                if m:
+                    datestring = m.group('month') + " " + ("%02d" % int(m.group('day'))) + " " + m.group('hour') + ":" + m.group('minute') + ":" + m.group('second')
+                    date = datetime.datetime.strptime(datestring, "%b %d %H:%M:S")
+                    timestamp = calendar.timegm(date_obj.timetuple())
+                    if timestamp >= start_time:
+                        results[index] = True
+                        return
+    except KeyboardInterrupt:
+        results[index] = False
+    except Exception as e:
+        results[index] = False
+
+def main():
+    global mvip, username, password, ssh_user, ssh_pass, timeout
+
+    # Pull in values from ENV if they are present
+    env_enabled_vars = [ "mvip", "username", "password" ]
+    for vname in env_enabled_vars:
+        env_name = "SF" + vname.upper()
+        if os.environ.get(env_name):
+            globals()[vname] = os.environ[env_name]
+
+    # Parse command line arguments
+    parser = OptionParser()
+    parser.add_option("--mvip", type="string", dest="mvip", default=mvip, help="the management IP of the cluster")
+    parser.add_option("--user", type="string", dest="username", default=username, help="the admin account for the cluster")
+    parser.add_option("--pass", type="string", dest="password", default=password, help="the admin password for the cluster")
+    parser.add_option("--ssh_user", type="string", dest="ssh_user", default=ssh_user, help="the SSH username for the nodes.  Only used if you do not have SSH keys set up")
+    parser.add_option("--ssh_pass", type="string", dest="ssh_pass", default=ssh_pass, help="the SSH password for the nodes.  Only used if you do not have SSH keys set up")
+    parser.add_option("--timeout", type="int", dest="timeout", default=timeout, help="")
+    parser.add_option("--debug", action="store_true", dest="debug", help="display more verbose messages")
+    (options, args) = parser.parse_args()
+    mvip = options.mvip
+    username = options.username
+    password = options.password
+    ssh_user = options.ssh_user
+    ssh_pass = options.ssh_pass
+    timeout = options.timeout
+    timeout = timeout * 60
+    
+    if options.debug != None:
+        import logging
+        mylog.console.setLevel(logging.DEBUG)
+    if not libsf.IsValidIpv4Address(mvip):
+        mylog.error("'" + mvip + "' does not appear to be a valid MVIP")
+        sys.exit(1)
+
+    mylog.info("Checking cluster version on " + mvip)
+    
+    result = libsf.CallApiMethod(mvip, username, password, "GetClusterVersionInfo", {})
+    if not result["clusterVersion"].startswith("5"):
+        mylog.error("This script only works with Boron and later")
+        sys.exit(1)
+    if float(result["clusterVersion"]) < 5.735:
+        mylog.error("This script only works with build 735 or later")
+        sys.exit(1)
+    
+    mylog.info("Getting a list of nodes in the cluster")
+    node_ip_list = []
+    result = libsf.CallApiMethod(mvip, username, password, "ListActiveNodes", {})
+    for node in result["nodes"]:
+        node_ip_list.append(node["mip"])
+    
+    mylog.info("Starting ClusterBSCheck")
+    start_time = time.time()
+    time.sleep(2)
+    result = libsf.CallApiMethod(mvip, username, password, "StartClusterBSCheck", {}, ApiVersion=5.0)
+    
+    mylog.info("Waiting for check to finish")
+    manager = multiprocessing.Manager()
+    results = manager.dict()
+    current_threads = []
+    thread_index = 0
+    for node_ip in node_ip_list:
+        results[thread_index] = False
+        th = multiprocessing.Process(target=WaitForBsCheckThread, name="Node-" + node_ip + "-" + str(thread_index), args=(node_ip, ssh_user, ssh_pass, start_time, timeout, results, thread_index))
+        th.start()
+        current_threads.append(th)
+        thread_index += 1
+    # Wait for all threads to stop
+    for th in current_threads:
+        th.join()
+    time.sleep(60)
+    
+    mylog.info("Looking for xUnknownBlockID")
+    
+    # Start one thread per node
+    mylog.info("Gathering information from nodes")
+    manager = multiprocessing.Manager()
+    results = manager.dict()
+    current_threads = []
+    thread_index = 0
+    for node_ip in node_ip_list:
+        results[thread_index] = False
+        th = multiprocessing.Process(target=NodeThread, name="Node-" + node_ip + "-" + str(thread_index), args=(node_ip, ssh_user, ssh_pass, results, thread_index))
+        th.start()
+        current_threads.append(th)
+        thread_index += 1
+        #if thread_index > 1: break
+
+    # Wait for all threads to stop
+    for th in current_threads:
+        th.join()
+
+    mylog.debug("Getting a list of accounts in the cluster")
+    account_map = dict()
+    result = libsf.CallApiMethod(mvip, username, password, "ListAccounts", {})
+    for account in result["accounts"]:
+        acc_id = account["accountID"]
+        acc_name = account["username"]
+        account_map[acc_id] = acc_name
+    
+    mylog.debug("Getting a list of volumes in the cluster")
+    vol_map = dict()
+    result = libsf.CallApiMethod(mvip, username, password, "ListActiveVolumes", {})
+    for vol in result["volumes"]:
+        vol_name = vol["name"]
+        vol_id = vol["volumeID"]
+        acc_id = vol["accountID"]
+        vol_map[vol_id] = dict()
+        vol_map[vol_id]["volumeID"] = vol_id
+        vol_map[vol_id]["accountID"] = acc_id
+        vol_map[vol_id]["volumeName"] = vol_name
+        vol_map[vol_id]["accountName"] = account_map[acc_id]
+
+    # Collate the results
+    broken_volumes = dict()
+    error = False
+    for res in results.values():
+        if res == False:
+            error = True
+        else:
+            for block in res:
+                slice_id = block["sliceID"]
+                if slice_id not in broken_volumes:
+                    broken_volumes[slice_id] = []
+                broken_volumes[slice_id].append(block)
+    
+    for volume_id, block_list in broken_volumes.iteritems():
+        volume_name = vol_map[volume_id]["volumeName"]
+        account_name = vol_map[volume_id]["accountName"]
+        account_id = vol_map[volume_id]["acountID"]
+        mylog.error("xUnknownBlockID on volume " + volume_name + " (" + volume_id + ") from account " + account_name + " (" + account_id + ")")
+        for block in block_list:
+            mylog.error("  blockID " + block["blockID"] + " from BS " + block["bs"] + " for LBA " + block["lba"])
+    
+    if len(broken_volumes) <= 0 and not error:
+        mylog.passed("No xUnknownBlockID found")
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    mylog.debug("Starting " + str(sys.argv))
+    try:
+        main()
+    except SystemExit:
+        raise
+    except KeyboardInterrupt:
+        mylog.warning("Aborted by user")
+        exit(1)
+    except:
+        mylog.exception("Unhandled exception")
+        exit(1)
+    exit(0)
+
---
+++ move_volume_to_account.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python
+
+# This script moves volumes from one account to another
+
+# ----------------------------------------------------------------------------
+# Configuration
+#  These may also be set on the command line
+
+mvip = "192.168.000.000"    # The management VIP of the cluster
+                            # --mvip
+
+username = "admin"          # Admin account for the cluster
+                            # --user
+
+password = "solidfire"      # Admin password for the cluster
+                            # --pass
+
+volume_name = ""            # The name of the volume to move
+                            # --volume_name
+
+volume_id = 0               # The volumeID of the volume to move
+                            # --volume_id
+
+volume_prefix = ""          # Prefix for the volumes to move
+                            # volume_name or volume_id will supercede this
+                            # --volume_prefix
+
+volume_regex = ""           # Regex to search for volumes to move
+                            # --volume_regex
+
+volume_count = 0            # move at most this many volumes (0 to move all matches)
+                            # --volume_count
+
+source_account = ""         # Account to use to search for volumes to move
+                            # Can be used with volume_prefix
+                            # volume_name or volume_id will supercede this
+                            # --source_account
+
+dest_account = ""           # Account to move the volumes to
+                            # --dest_account
+
+# ----------------------------------------------------------------------------
+
+
+import sys,os
+from optparse import OptionParser
+import json
+import urllib2
+import random
+import platform
+import time
+import libsf
+from libsf import mylog, SfError
+
+def main():
+    global mvip, username, password, source_account, dest_account, volume_id, volume_name, volume_id, volume_prefix, volume_regex, volume_count
+
+    # Pull in values from ENV if they are present
+    env_enabled_vars = [ "mvip", "username", "password" ]
+    for vname in env_enabled_vars:
+        env_name = "SF" + vname.upper()
+        if os.environ.get(env_name):
+            globals()[vname] = os.environ[env_name]
+
+    # Parse command line arguments
+    parser = OptionParser()
+    parser.add_option("--mvip", type="string", dest="mvip", default=mvip, help="the management IP of the cluster")
+    parser.add_option("--user", type="string", dest="username", default=username, help="the admin account for the cluster")
+    parser.add_option("--pass", type="string", dest="password", default=password, help="the admin password for the cluster")
+    parser.add_option("--volume_name", type="string", dest="volume_name", default=volume_name, help="the volume to move")
+    parser.add_option("--volume_id", type="int", dest="volume_id", default=volume_id, help="the volume to move")
+    parser.add_option("--volume_prefix", type="string", dest="volume_prefix", default=volume_prefix, help="the prefix of volumes to move")
+    parser.add_option("--volume_regex", type="string", dest="volume_regex", default=volume_regex, help="regex to search for volumes to move")
+    parser.add_option("--volume_count", type="int", dest="volume_count", default=volume_count, help="the number of volumes to move")
+    parser.add_option("--source_account", type="string", dest="source_account", default=source_account, help="the name of the account to select volumes from")
+    parser.add_option("--dest_account", type="string", dest="dest_account", default=dest_account, help="the name of the account to move volumes to")
+    parser.add_option("--test", action="store_true", dest="test", help="show the volumes that would be moved but don't actually move them")
+    parser.add_option("--debug", action="store_true", dest="debug", help="display more verbose messages")
+    (options, args) = parser.parse_args()
+    mvip = options.mvip
+    username = options.username
+    password = options.password
+    volume_name = options.volume_name
+    volume_id = options.volume_id
+    volume_prefix = options.volume_prefix
+    volume_regex = options.volume_regex
+    volume_count = options.volume_count
+    source_account = options.source_account
+    dest_account = options.dest_account
+    if options.debug != None:
+        import logging
+        mylog.console.setLevel(logging.DEBUG)
+    if options.test:
+        test = True
+    else:
+        test = False
+    if not libsf.IsValidIpv4Address(mvip):
+        mylog.error("'" + mvip + "' does not appear to be a valid MVIP")
+        sys.exit(1)
+
+    # Find the destination account
+    accounts_list = libsf.CallApiMethod(mvip, username, password, "ListAccounts", {})
+    dest_account_id = 0
+    for account in accounts_list["accounts"]:
+        if account["username"].lower() == dest_account.lower():
+            dest_account_id = account["accountID"]
+            break
+    if dest_account_id <= 0:
+        mylog.error("Could not find account '" + dest_account + "'")
+        sys.exit(1)
+    
+    
+    # Get a list of volumes to move
+    mylog.info("Searching for volumes")
+    try:
+        volumes_to_move = libsf.SearchForVolumes(mvip, username, password, VolumeId=volume_id, VolumeName=volume_name, VolumeRegex=volume_regex, VolumePrefix=volume_prefix, AccountName=source_account, VolumeCount=volume_count)
+    except SfError as e:
+        mylog.error(e.message)
+        sys.exit(1)
+
+    count = len(volumes_to_move.keys())
+    names = ", ".join(sorted(volumes_to_move.keys()))
+    mylog.info(str(count) + " volumes wil be moved: " + names)
+
+    if test:
+        mylog.info("Test option set; volumes will not be moved")
+        sys.exit(0)
+
+    # move the requested volumes
+    mylog.info("Moving volumes")
+    for vol_name in sorted(volumes_to_move.keys()):
+        vol_id = volumes_to_move[vol_name]
+        mylog.debug("Moving " + vol_name)
+        params = {}
+        params["volumeID"] = vol_id
+        params["accountID"] = dest_account_id
+        volume_obj = libsf.CallApiMethod(mvip, username, password, "ModifyVolume", params, ApiVersion=5.0)
+
+    mylog.passed("Successfully moved volumes")
+
+if __name__ == '__main__':
+    mylog.debug("Starting " + str(sys.argv))
+    try:
+        main()
+    except SystemExit:
+        raise
+    except KeyboardInterrupt:
+        mylog.warning("Aborted by user")
+        exit(1)
+    except:
+        mylog.exception("Unhandled exception")
+        exit(1)
+    exit(0)
