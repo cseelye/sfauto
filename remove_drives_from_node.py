65fd26fd537941ca9af90ae74ae5c8a4f2a3af8c
Carl Seelye <cseelye@gmail.com>
2013-04-08 10:35:07 -0600

    remove_drives_from_node - allow specifying multiple nodes

--- remove_drives_from_node.py
+++ remove_drives_from_node.py
@@ -42,18 +42,19 @@ def main():
     parser.add_option("--mvip", type="string", dest="mvip", default=mvip, help="the management IP of the cluster")
     parser.add_option("--user", type="string", dest="username", default=username, help="the admin account for the cluster")
     parser.add_option("--pass", type="string", dest="password", default=password, help="the admin password for the cluster")
-    parser.add_option("--node_ip", type="string", dest="node_ip", default=node_ip, help="the mIP of the node to remove")
+    parser.add_option("--node_ip", type="string", dest="node_ip", default=node_ip, help="the management IP(s) of the node(s) to remove")
     parser.add_option("--debug", action="store_true", dest="debug", help="display more verbose messages")
     (options, args) = parser.parse_args()
     mvip = options.mvip
     username = options.username
     password = options.password
-    node_ip = options.node_ip
+    try:
+        node_ips = libsf.ParseIpsFromList(options.node_ip)
+    except TypeError as e:
+        mylog.error(e)
+        sys.exit(1)
     if not libsf.IsValidIpv4Address(mvip):
         mylog.error("'" + mvip + "' does not appear to be a valid MVIP")
-        sys.exit(1)
-    if not libsf.IsValidIpv4Address(node_ip):
-        mylog.error("'" + node_ip + "' does not appear to be a node IP")
         sys.exit(1)
     if options.debug != None:
         import logging
@@ -62,28 +63,28 @@ def main():
 
     # Find the nodeID of the requested node
     mylog.info("Searching for nodes")
-    node_id = 0
+    node_ids = []
     result = libsf.CallApiMethod(mvip, username, password, "ListActiveNodes", {})
-    for node in result["nodes"]:
-        if node["mip"] == node_ip:
-            node_id = node["nodeID"]
-            break
-    if node_id <= 0:
-        mylog.error("Could not find node " + node_ip)
-        sys.exit(1)
-    mylog.info("Found node " + node_ip + " is nodeID " + str(node_id))
+    for node_ip in node_ips:
+        found = False
+        for node in result["nodes"]:
+            if node["mip"] == node_ip:
+                node_ids.append(node["nodeID"])
+                found = True
+                break
+        if not found:
+            mylog.error("Could not find node " + node_ip + " in cluster " + mvip)
+            sys.exit(1)
 
-    # Remove all active and failed drives from the node
+    # Remove all active and failed drives from the nodes
     mylog.info("Searching for drives")
     drives_to_remove = []
     result = libsf.CallApiMethod(mvip, username, password, "ListDrives", {})
     for drive in result["drives"]:
-        if (drive["status"].lower() == "active" or drive["status"].lower() == "active") and node_id == drive["nodeID"]:
+        if (drive["status"].lower() == "active" or drive["status"].lower() == "failed") and drive["nodeID"] in node_ids:
             drives_to_remove.append(drive["driveID"])
     if len(drives_to_remove) > 0:
         mylog.info("Removing " + str(len(drives_to_remove)) + " drives " + str(drives_to_remove))
-        remove_time = time.time()
-        time.sleep(2)
         libsf.CallApiMethod(mvip, username, password, "RemoveDrives", {'drives': drives_to_remove})
 
         mylog.info("Waiting for syncing")
@@ -94,6 +95,10 @@ def main():
         # Wait for slice syncing
         while libsf.ClusterIsSliceSyncing(mvip, username, password):
             time.sleep(30)
+    else:
+        mylog.info("Found no drives to remove")
+
+    mylog.passed("Successfully removed drives from node(s)")
 
 
 
