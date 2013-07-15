7041f17a47240da61376a13d7415ea9c2b609c6e
Carl Seelye <cseelye@gmail.com>
2013-07-15 10:20:19 -0600

    Win compat - scripts will now work standalone, but shared values will not

--- lib/datastore.py
+++ lib/datastore.py
@@ -6,19 +6,28 @@ Global storage for all actions to share
 
 # Using a multiprocessing dict allows any process/thread to update this and still be visible across all processes
 # Unless someone does something strange, this module is implicitly imported long before any forks, so this will always be instantiated and accessable across processes.
-import multiprocessing
-__manager = multiprocessing.Manager()
-__storage = __manager.dict()
-__lock = multiprocessing.Lock()
+import platform
+if "windows" in platform.system().lower():
+    __storage = dict()
+    __lock = None
+else:
+    import multiprocessing
+    __manager = multiprocessing.Manager()
+    __storage = __manager.dict()
+    __lock = multiprocessing.Lock()
 
 def Dump():
     for key in sorted(__storage.keys()):
         print key + " => " + str(__storage[key])
 
 def Lock():
+    if "windows" in platform.system().lower():
+        return
     __lock.acquire()
 
 def Unlock():
+    if "windows" in platform.system().lower():
+        return
     __lock.release()
 
 def Set(keyName, value):
