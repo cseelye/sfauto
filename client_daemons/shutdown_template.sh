c2788329f77f1970bdc44e646f0ee51c79dcd5f1
Carl Seelye <cseelye@gmail.com>
2013-09-24 17:00:21 -0500

    Fix file path in shutdown template script

--- client_daemons/shutdown_template.sh
+++ client_daemons/shutdown_template.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "========= Shutting down template VM =========" > /var/log/syslog
-rm -f /opt/firstboot/firstbootdone
+rm -f /opt/sfauto/client_daemons/firstbootdone
 shutdown -h now
