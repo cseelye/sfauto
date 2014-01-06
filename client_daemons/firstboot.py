598e2ff9b07ec381002ac5e78d8256a79931cdc7
Carl Seelye <cseelye@gmail.com>
2014-01-06 15:23:48 -0700

    Make firstboot.py more robust, support more hypervisor configurations

--- client_daemons/firstboot.py
+++ client_daemons/firstboot.py
@@ -2,6 +2,7 @@ import os
 import platform
 import re
 import sys
+from xml.etree import ElementTree
 sys.path.append("..")
 import lib.libsf as libsf
 from lib.libsf import mylog
@@ -21,72 +22,77 @@ if os.path.exists(donefile):
     mylog.info("Skipping firstboot setup because firstboot has alread been run")
     sys.exit(0)
 
-# Try to figure out what hypervisor we are running on
 mylog.info("Detecting hypervisor")
 hypervisor = libsf.GuessHypervisor().lower()
 
+# Make sure we are not on a physical machine
+if "physical" in hypervisor:
+    mylog.info("Skipping firstboot because this machine does not look like a VM guest.")
+    sys.exit(0)
+
 # Get the name of my virtual machine container from the hypervisor
 new_hostname = ""
-if "vmware" in hypervisor or "esx" in hypervisor:
-    mylog.info("Detected we are a VMware guest")
-    if windows:
+
+# Try VMware
+if not new_hostname:
+    command = None
+    if windows and os.path.exists("C:\Program Files\VMware\VMware Tools\vmtoolsd.exe"):
         command = r'"C:\Program Files\VMware\VMware Tools\vmtoolsd.exe" --cmd "info-get guestinfo.hostname"'
-    else:
+    elif os.path.exists("/usr/sbin/vmtoolsd"):
         command = '/usr/sbin/vmtoolsd --cmd "info-get guestinfo.hostname" 2>/dev/null'
-    retcode, stdout, stderr = libsf.RunCommand(command)
-    if retcode == 0:
-        new_hostname = stdout.strip()
-        mylog.info("My VM name is " + new_hostname)
+    if command:
+        retcode, stdout, stderr = libsf.RunCommand(command)
+        if retcode == 0:
+            mylog.info("Detected we are a VMware guest")
+            new_hostname = stdout.strip()
+            mylog.info("My VM name is " + new_hostname)
 
-elif "xen" in hypervisor:
-    mylog.info("Detected we are a Xen guest")
+# Try XenServer
+if not new_hostname:
+    command = None
     if windows:
-        command = 'powershell -file xenstore_getname.ps1'
-    else:
+        command = 'powershell -NoProfile -NonInteractive -NoLogo -ExecutionPolicy Unrestricted -Command ./xenstore_getname.ps1'
+    elif os.path.exists("/usr/bin/xenstore-read"):
         command = '/usr/bin/xenstore-read name 2>/dev/null'
-    retcode, stdout, stderr = libsf.RunCommand(command)
-    if retcode == 0:
-        new_hostname = stdout.strip()
-        mylog.info("My VM name is " + new_hostname)
+    if command:
+        retcode, stdout, stderr = libsf.RunCommand(command)
+        if retcode == 0:
+            mylog.info("Detected we are a Xen guest")
+            new_hostname = stdout.strip()
+            mylog.info("My VM name is " + new_hostname)
 
-elif "hyperv" in hypervisor:
-    mylog.info("Detected we are a HyperV guest")
+# Try HyperV
+if not new_hostname:
     if windows:
         command = r'reg query "HKLM\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters"  /v VirtualMachineName'
         retcode, stdout, stderr = libsf.RunCommand(command)
         if retcode == 0:
+            mylog.info("Detected we are a HyperV guest")
             for line in stdout.split("\n"):
                 line = line.strip()
                 if len(line) <= 0: continue
                 if line.startswith("VirtualMachineName"):
-                    pieces = re.split("\s+", line)
+                    pieces = re.split(r"\s+", line)
                     new_hostname = pieces[2]
                     break
             mylog.info("My VM name is " + new_hostname)
+        else:
+            mylog.error("Could not determine my VM name")
+            sys.exit(1)
     else:
-        command = r"/bin/cat /var/opt/hyperv/.kvp_pool_3 | /bin/sed 's/\x0/ /g' | /usr/bin/awk '{print $20}'"
-        retcode, stdout, stderr = libsf.RunCommand(command)
-        if retcode == 0:
-            new_hostname = stdout.strip()
-            mylog.info("My VM name is " + new_hostname)
+        if os.path.exists("/var/opt/hyperv/.kvp_pool_3"):
+            command = r"/bin/cat /var/opt/hyperv/.kvp_pool_3 | /bin/sed 's/\x0/ /g' | /usr/bin/awk '{print $20}'"
+            retcode, stdout, stderr = libsf.RunCommand(command)
+            if retcode == 0:
+                mylog.info("Detected we are a HyperV guest")
+                new_hostname = stdout.strip()
+                mylog.info("My VM name is " + new_hostname)
 
-elif "kvm" in hypervisor:
-    mylog.info("Detected we are a KVM guest")
-
-elif "physical" in hypervisor:
-    mylog.info("This machine does not look like a VM guest.")
-    sys.exit(0)
-
-else:
-    mylog.info("Unrecognized hypervisor")
-
-
-# Generate a hostname from the MAC address
+# Try more complex methods
 if not new_hostname:
-    mylog.info("Generating unique hostname from MAC address")
+    mylog.info("Getting a list of my MAC addresses")
     mac_list = []
     if windows:
-        hostname_prefix = "win-"
         retcode, stdout, stderr = libsf.RunCommand("getmac.exe /v /nh /fo csv")
         for line in stdout.split("\n"):
             line = line.strip()
@@ -96,15 +102,9 @@ if not new_hostname:
             if mac == "000000000000": continue # Occasionally we see MACs that are all 0 from badly configured NICs/bonds
             mac_list.append(mac)
     else:
-        if "ubuntu" in platform.linux_distribution()[0].lower():
-            hostname_prefix = "ubuntu-"
-        elif "el" in platform.release():
-            hostname_prefix = "rhel-"
-        else:
-            hostname_prefix = "unix-"
         retcode, stdout, stderr = libsf.RunCommand("ifconfig | grep HWaddr | awk '{print $5}' | sed 's/://g' | sort -u")
         for mac in stdout.split("\n"):
-            mac = mac.strip()
+            mac = mac.strip().lower()
             if len(mac) <= 0: continue
             if mac == "000000000000": continue # Occasionally we see MACs that are all 0 from badly configured NICs/bonds
             mac_list.append(mac)
@@ -113,7 +113,104 @@ if not new_hostname:
         mac_addr = mac_list[0]
     else:
         mac_addr = "UNKNOWN"
-    new_hostname = hostname_prefix + mac_addr
+
+    # If we are running on VMware, connect to the vCenter server and look for a VM with my MAC address
+    if "vmware" in hypervisor or "esx" in hypervisor:
+        try:
+            from pysphere import VIServer
+        except ImportError:
+            pass
+        else:
+            mylog.info("Searching vCenter for a VM with my MAC address")
+            vcenter = VIServer()
+            vcenter.connect("vcenter.domain.local", "script_user", "password")
+            try:
+                vmlist = vcenter.get_registered_vms(status='poweredOn')
+                vm_name = None
+                for vm in vmlist:
+                    net = vm.get_property('net', from_cache=False)
+                    if net:
+                        for interface in net:
+                            mac = interface.get('mac_address', None)
+                            if mac:
+                                mac = mac.replace(":", "").lower()
+                            if mac in mac_list:
+                                vm_name = vm.get_property('name')
+                                break
+                    if vm_name:
+                        break
+                if vm_name:
+                    mylog.info("My VM name is " + vm_name)
+                    new_hostname = vm_name
+            finally:
+                vcenter.disconnect()
+
+    # If we are running on KVM, connect to each KVM server and look for a VM with my MAC address
+    if "kvm" in hypervisor:
+        try:
+            import socket
+            import libvirt
+        except ImportError:
+            pass
+        else:
+            mylog.info("Searching KVM hosts for a VM with my MAC address")
+            socket.setdefaulttimeout(10)
+            hostname_prefix = "kvm"
+            for i in xrange(1, 16):
+                vmhost = hostname_prefix + str(i)
+                try:
+                    vmhost_ip = socket.gethostbyname(vmhost)
+                except socket.gaierror:
+                    # Could not resolve name
+                    break
+
+                try:
+                    conn = libvirt.openReadOnly("qemu+tcp://" + vmhost_ip + "/system")
+                except libvirt.libvirtError as e:
+                    mylog.error("Failed to connect to " + vmhost_ip + ": " + str(e))
+                    continue
+
+                try:
+                    vm_ids = conn.listDomainsID()
+                    running_vm_list = map(conn.lookupByID, vm_ids)
+                    running_vm_list = sorted(running_vm_list, key=lambda vm: vm.name())
+                except libvirt.libvirtError as e:
+                    mylog.error("Failed to get VM list: " + str(e))
+                    continue
+
+                found = None
+                for vm in running_vm_list:
+                    vm_xml = ElementTree.fromstring(vm.XMLDesc(0))
+                    vm_mac_list = []
+
+                    for node in vm_xml.findall("devices/interface/mac"):
+                        mac = node.get("address")
+                        if mac:
+                            mac = mac.replace(":", "").lower()
+                            vm_mac_list.append(mac)
+                    for mac in mac_list:
+                        if mac in vm_mac_list:
+                            found = vm
+                            break
+                if found:
+                    break
+            if found:
+                mylog.info("My VM name is " + found.name())
+                new_hostname = found.name()
+
+    # If all else fails, generate a hostname from my MAC address and OS type
+    if not new_hostname:
+        mylog.info("Generating unique hostname from MAC address")
+        if windows:
+            hostname_prefix = "win-"
+        else:
+            if "ubuntu" in platform.linux_distribution()[0].lower():
+                hostname_prefix = "ubuntu-"
+            elif "el" in platform.release():
+                hostname_prefix = "rhel-"
+            else:
+                hostname_prefix = "unix-"
+        new_hostname = hostname_prefix + mac_addr
 
 
 # See if we are a template VM
@@ -128,7 +225,7 @@ if windows:
     for line in stdout.split("\n"):
         line = line.strip()
         if len(line) <= 0: continue
-        m = re.search("^\s*(\d+)\s+(\d+)\s+(\d+)\s+(\S+)\s+(.+)", line)
+        m = re.search(r"^\s*(\d+)\s+(\d+)\s+(\d+)\s+(\S+)\s+(.+)", line)
         if m:
             index = int(m.group(1))
             #metric = int(m.group(2))
--- client_daemons/xenstore_getname.ps1
+++ client_daemons/xenstore_getname.ps1
@@ -1,3 +1,6 @@
+$ErrorActionPreference = "Stop"
+$WarningPreference = "Stop"
+$ProgressPreference = "SilentlyContinue"
 $base = gwmi -n root\wmi -cl CitrixXenStoreBase
 $sid = $base.AddSession("a")
 $s = gwmi -n root\wmi -q "select * from CitrixXenStoreSession where sessionid=$($sid.SessionId)"
